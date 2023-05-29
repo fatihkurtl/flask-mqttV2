5f5e11bdd79b59d15f8d0f9647f6ec8e334122f9
Fatih Kurt <101009145+fatihkurtl@users.noreply.github.com>
2023-05-29 16:45:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from time import time
 import datetime #
 
 a = currentDateTime = datetime.datetime.now() #
-# a = str(currentDateTime)
-# aevsis123 wifi
+
 MQTT_HOST = '10.0.0.2' #raspberrypi.local / 10.0.0.2
 MQTT_PORT = 1883
 MQTT_CLIENT_ID = 'Python MQTT client'
@@ -19,17 +18,15 @@ DATABASE_FILE = 'mqtt.db'
 
 def on_connect(mqtt_client, user_data, flags, conn_result):
     mqtt_client.subscribe(TOPIC)
-    #mqtt_client.subscribe(TOPIC2)
 
 def on_message(mqtt_client, user_data, message):
     print(message.payload.decode('utf-8'))
     payload = message.payload.decode('utf-8')
 
     db_conn = user_data['db_conn']
-    # sql = 'INSERT INTO raspberry_data (topic, payload, time) VALUES (?, ?, ?)'
     sql = 'INSERT INTO raspberry_data (topic, payload, time) VALUES (?, ?, datetime("now", "localtime"))'
     cursor = db_conn.cursor()
-    cursor.execute(sql, (message.topic, payload)) # a buraya
+    cursor.execute(sql, (message.topic, payload))
     db_conn.commit()
     cursor.close()
 
@@ -44,30 +41,10 @@ def main():
         time DATETIME DEFAULT CURRENT_TIMESTAMP
     )
     """
-    # sql = """
-    # CREATE TABLE IF NOT EXISTS raspberry_data (
-    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #     topic TEXT NOT NULL,
-    #     payload TEXT NOT NULL,
-    #     created_at INTEGER NOT NULL
-    # )
-    # """
 
-    ### 2. database
-    # sql2 = """
-    # CREATE TABLE IF NOT EXISTS temperature (
-    #     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #     topic TEXT NOT NULL,
-    #     payload TEXT NOT NULL,
-    #     temperature TEXT NOT NULL,
-    #     time DATETIME DEFAULT CURRENT_TIMESTAMP
-    # )
-    # """
-    ###
 
     cursor = db_conn.cursor()
     cursor.execute(sql)
-    # cursor.execute(sql2)# 2. database saved
     cursor.close()
 
     mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
